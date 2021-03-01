db2dc14f40325501c666a7a013543b84d1c9bf48
AbrarHasin <abrarhasinpranto@gmail.com>
2021-03-01 19:33:05 +0600

    added filters and search field in admin questions page

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import datetime
-from django.utils import timezone
+from django.utils import timezone, tree
 
 # Create your models here.
 
@@ -13,6 +13,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
         
 
 class Choice(models.Model):
